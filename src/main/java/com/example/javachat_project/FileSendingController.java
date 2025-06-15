37b46fb72df737b24a1407e9ed8eea72adcf9ebc
mshupliakou <mikhail.shupliakou@gmail.com>
2025-06-15 15:22:08 +0200

    Controller for sending files

--- src/main/java/com/example/javachat_project/FileSendingController.java
+++ src/main/java/com/example/javachat_project/FileSendingController.java
@@ -1,28 +1,45 @@
 package com.example.javachat_project;
 
 import javafx.fxml.FXML;
+
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
-import java.io.BufferedReader;
 import java.io.File;
 
 public class FileSendingController {
-    @FXML private ImageView image;
     @FXML private Button sendButton;
+    @FXML private Label file_name_label;
+    @FXML private Label file_size_label;
+
+    public void setText_input_file_sending(String text) {
+        text_input_file_sending.setText(text);
+    }
+
+    @FXML private TextField text_input_file_sending;
 
     private File file;
     private boolean sendConfirmed = false;
 
+    public static String readableFileSize(long size) {
+        if (size <= 0) return "0 B";
+        final String[] units = new String[]{"B", "KB", "MB", "GB", "TB"};
+        int digitGroups = (int) (Math.log10(size) / Math.log10(1024));
+        return String.format("%.1f %s", size / Math.pow(1024, digitGroups), units[digitGroups]);
+    }
     public void setFile(File new_file){
         file=new_file;
-    }
+        if (file != null) {
+            file_name_label.setText(file.getName());
+            file_size_label.setText( readableFileSize(file.length()));
+        } else {
+            file_name_label.setText("File is not chosen");
+            file_size_label.setText("");
+        }
 
-    public void setImageForSending(Image new_image){
-        image.setImage(new_image);
     }
 
     @FXML
@@ -37,5 +54,9 @@ public class FileSendingController {
 
     public void handelCancel(MouseEvent mouseEvent) {
         ((Stage) sendButton.getScene().getWindow()).close();
+    }
+
+    public String getText_input_file_sending() {
+        return text_input_file_sending.getText();
     }
 }
